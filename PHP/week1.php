0e33b7e12aae6a164309e8fbda0178728f8bf3ad
Dian Rezky Wulandari <dianrezky03@gmail.com>
2023-05-25 22:10:17 +0700

    Update week1.php

--- PHP/week1.php
+++ PHP/week1.php
@@ -536,7 +536,7 @@ foreach ($lines as $line) {
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">The Office Income</div>
+                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">The Office Incnnome</div>
                                             <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo "Rp. " . number_format($total_payment, 2, ".", ".") ?></div>
                                         </div>
                                         <div class="col-auto">
